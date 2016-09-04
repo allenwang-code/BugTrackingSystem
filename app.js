c12a7d23a321beb07a796b8cdb102195f0f7d612
AllenWang <s972024@gmail.com>
2016-09-04 20:32:11 +0800

    use pug(named ejs before) to render view

--- app.js
+++ app.js
@@ -2,10 +2,17 @@ var express = require('express');
 var app = express();
 
 app.use('/static', express.static('public'));
+app.set('views', './views');
+app.set('view engine', 'pug');
 
 app.get('/', function (req, res) {
-  res.send('Great to see you again, dude');
+  res.render('index', { title:'hey!', message:'Hi, There!'});
+  //res.send('Great to see you again, dude');
 });
+
+
+
+
 
 app.listen(3000, function() {
   console.log('Allen Run in port 3000!');
